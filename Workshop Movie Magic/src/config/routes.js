029076b125ace9f15bb83f5723e555017d1da251
psstoykov <145485240+psstoykov@users.noreply.github.com>
2024-06-08 13:42:29 +0200

    main template structure

--- Workshop Movie Magic/src/config/routes.js
+++ Workshop Movie Magic/src/config/routes.js
@@ -1,7 +1,20 @@
 const { Router } = require('express');
+const { home, details } = require('../controllers/catalogue');
+const { about } = require('../controllers/about');
+const { createGet, createPost } = require('../controllers/movie');
+const { notFound } = require('../controllers/404');
+const { search } = require('../controllers/catalogue');
 
 const router = Router();
 
-//TODO add router
+router.get('/', home);
+router.get('/details/:id', details);
+router.get('/about', about);
+router.get('/create', createGet)
+router.post('/create', createPost);
+router.get('/search', search);
+
+router.get('*', notFound);
+
 
 module.exports = { router };
---
+++ Workshop Movie Magic/src/controllers/movie.js
@@ -0,0 +1,8 @@
+module.exports = {
+    createGet: (req, res) => {
+        res.render('create')
+    },
+    createPost: (req, res) => {
+        //TODO createPost action
+    }
+}
