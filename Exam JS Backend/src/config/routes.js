99d605d6293f8b193d8af13a1111b3baf0d4d329
psstoykov <145485240+psstoykov@users.noreply.github.com>
2024-06-23 12:08:04 +0200

    todo's

--- Exam JS Backend/src/config/routes.js
+++ Exam JS Backend/src/config/routes.js
@@ -5,9 +5,10 @@ const { session } = require('../middlewares/session');
 
 
 function configRoutes(app) {
-    //TODO attach the specific routes to the app with app.get or app.use(external router from controller)
+    //TODO attach the specific routes to the app with app.get or app.use(external router from controllers)
 
     app.use(userRouter);
+
     app.get('*', (req, res) => {
         res.render('404')
     })
